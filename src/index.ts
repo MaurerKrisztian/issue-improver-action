3f5d890472875011b97672c92406efaaaa42e05e
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2023-04-26 19:16:53 +0200

    feat: related issues

--- src/index.ts
+++ src/index.ts
@@ -37,16 +37,38 @@ async function run() {
 
     core.notice(`[GPT MESSAGE]: ${gptMessage}`);
 
+    const issuesResponse = await octokit.rest.issues.listForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        state: 'open',
+    });
+
+    const issues = issuesResponse.data.map((issue) => ({
+        number: issue.number,
+        title: issue.title,
+        link: issue.html_url,
+    }));
+
+    const relatedIssuesResponse = await openaiClient.createCompletion({
+        model: model,
+        prompt: `Find related issues to: " {{issueTitle}} "  from thies issues: ${JSON.stringify(
+            issues,
+        )}. Make a list of issue title what is may related in this format [title](link)`,
+        max_tokens: maxTokens,
+    });
+    const gptRelatedIssuesMessage = relatedIssuesResponse.data.choices[0].text;
+
     core.notice(`Try to create a comment...`);
     await octokit.rest.issues.createComment({
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: issue?.number,
-        body: gptMessage,
+        body: gptMessage + '\n\n ' + gptRelatedIssuesMessage,
     });
 
     core.notice('Comment created successfully');
 }
+
 
 const runner = new ActionRunner({ name: 'GPT issue improver', cb: run });
 runner.run();
