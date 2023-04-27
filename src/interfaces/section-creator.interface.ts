114e24404f9d8b7997163265e07cd1a7e000c0fd
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2023-04-27 13:57:04 +0200

    fix: IInputs import

--- src/interfaces/section-creator.interface.ts
+++ src/interfaces/section-creator.interface.ts
@@ -1,10 +1,10 @@
-import { IInputs } from '../index';
 import { OpenAIApi } from 'openai';
 import { Octokit } from '@octokit/core';
 import { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types';
 import { PaginateInterface } from '@octokit/plugin-paginate-rest';
 import { ISection } from '../services/comment-builder';
 import { IConfig } from './config.interface';
+import { IInputs } from './inputs.interface';
 
 export interface ISectionCreator {
     isAddSection(inputs: IInputs, config?: Partial<IConfig>): boolean;
