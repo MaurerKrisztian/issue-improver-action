e2a40ffb85a476f41397482914089bbe13e5df39
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2023-05-06 21:02:47 +0200

    fix: better prompts #2

--- src/config/config-reader.ts
+++ src/config/config-reader.ts
@@ -1,30 +1,8 @@
 import fs from 'fs';
 import * as core from '@actions/core';
 import { IConfig } from '../interfaces/config.interface';
+import { defaultConfig } from './default-config';
 
-const defaultConfig: IConfig = {
-    sections: {
-        custom: [
-            {
-                title: '[JOKE]',
-                prompt: 'make a joke about this: {{issueTitle}}',
-            },
-        ],
-        relatedIssues: {
-            title: '[Related Issues]',
-            prompt: 'Find very similar related issue titles for " title: {{issueTitle}} "  from thies issues: {{openIssues}} . If none of them very similar just respond with a "none". Make a list of issue title what is related in this format [title](link) - [the similarity]',
-        },
-        summary: { title: '[Summary]', prompt: 'Summarize this github issue: {{issueTitle}} {{issueBody}}' },
-        labelSuggestion: {
-            title: '[Label Suggestion]',
-            prompt: 'Please make a label suggestion form this labels {{allLabels}} for this github issue: {{issueTitle}} {{issueBody}}.',
-        },
-        commentSummary: {
-            title: '[Comment summary]',
-            prompt: 'Summarize thies comments {{issueComments}} for this issue {{issueTitle}} {{issueBody}}',
-        },
-    },
-};
 export function getConfig(path?: string): Partial<IConfig> {
     const fileName = path || 'issue-improver-config.json';
     if (!fs.existsSync(fileName)) {
---
+++ src/config/default-config.ts
@@ -0,0 +1,23 @@
+import { IConfig } from '../interfaces/config.interface';
+
+export const defaultConfig: IConfig = {
+    sections: {
+        custom: [],
+        relatedIssues: {
+            title: 'Related Issues',
+            prompt: "From the list of open issues: {{openIssues}}, identify the most relevant ones related to '{{issueTitle}}' and provide a brief description of their similarities. Just the very simmilar related issues to '{{issueTitle}}' shoud be included in the answer, if none is very similar, andwer with 'none',",
+        },
+        summary: {
+            title: 'Summary',
+            prompt: "Provide a concise summary of the main points and objectives presented in the issue '{{issueTitle}}' and its content: {{issueBody}}.",
+        },
+        commentSummary: {
+            title: 'Comment summary',
+            prompt: "Review the comments in {{issueComments}} for the issue '{{issueTitle}}' and its content: {{issueBody}}. Extract the key takeaways, notable updates, and any consensus reached, and provide a concise summary of the discussion.",
+        },
+        labelSuggestion: {
+            title: 'Label Suggestion',
+            prompt: "Analyze the issue '{{issueTitle}}' and its content: {{issueBody}}, and suggest appropriate labels from the available labels {{allLabels}} that accurately represent the topic, scope, and complexity of the issue. The response shoud only include a label and why its suitable.",
+        },
+    },
+};
