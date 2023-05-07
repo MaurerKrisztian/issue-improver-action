2950a603046b8a13e7b207be7869422b3ccb95e9
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2023-05-07 21:46:00 +0200

    refactor: add warning log if placeholder can't be resolved

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,7 +1,15 @@
+import * as core from '@actions/core';
+
 export class Utils {
     static resolveTemplate(template: string, context: Record<string, string>) {
         return template.replace(/{{([^}]+)}}/g, (match, prop) => {
-            return context[prop] ?? `${match}`;
+            return (
+                context[prop] ??
+                (function () {
+                    core.warning(`Can't resolve ${match} value.`);
+                    return `${match}`;
+                })()
+            );
         });
     }
 }
